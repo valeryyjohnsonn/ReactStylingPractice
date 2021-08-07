fd53f34cc3e5d23244557f1267d73d5b8f8539b7
valeryyjohnsonn <49496668+valeryyjohnsonn@users.noreply.github.com>
2021-08-07 10:00:50 -0500

    Applying Dynamic Styling to a React Element

--- src/index.js
+++ src/index.js
@@ -5,3 +5,29 @@
 //Apply the "heading" style in the styles.css
 //Dynamically change the color of the h1 using inline css styles.
 //Morning = red, Afternoon = green, Night = blue.
+import React from "react";
+import ReactDOM from "react-dom";
+
+const currentTime = new Date().getHours();
+let timeOfDayColor = "";
+let timeOfDay = "";
+
+if (currentTime < 12 && currentTime >= 0) {
+  timeOfDay = "Morning";
+  timeOfDayColor = "red";
+} else if (currentTime > 12 && currentTime <= 18) {
+  timeOfDay = "Afternoon";
+  timeOfDayColor = "green";
+} else if (currentTime > 18 && currentTime <= 24) {
+  timeOfDay = "Evening";
+  timeOfDayColor = "blue";
+}
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading" style={{ color: timeOfDayColor }}>
+      Good {timeOfDay}!
+    </h1>
+  </div>,
+  document.getElementById("root")
+);
